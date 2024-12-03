4180ceb8b494ad405ddbc2e56bd4edeb8f8e96b5
Sawaiz <l201020@lhr.nu.edu.pk>
2024-12-03 22:11:09 +0500

    Test workflow 6

--- src/loginapp/LoginApp.java
+++ src/loginapp/LoginApp.java
@@ -74,8 +74,8 @@ public class LoginApp extends JFrame {
             e.printStackTrace();
         }
 
-        System.out.println("This is another print statement added for testing! #3");
-        System.out.println("This is another print statement added for testing! #3");
+        System.out.println("This is another print statement added for testing! #6");
+        System.out.println("This is another print statement added for testing! #6");
         return userName;
     }
 
